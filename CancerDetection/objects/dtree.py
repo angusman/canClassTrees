8a2060ad8e2d48bbf7ecc68ebb2d1921affe6434
Nick Sullivan <nbsullivan@gmail.com>
2016-07-05 15:43:20 -0700

    skelleton of dtree object

---
+++ CancerDetection/objects/dtree.py
@@ -0,0 +1,17 @@
+class dtree(object)
+	"""
+	Basic decision tree object
+	"""
+	def __init__(self, data = None):
+		if data != None:
+			self.load_data(data)
+
+	def load_data(self, data = None):
+		# load data here
+	
+	def predict(self, sample):
+		# predict the state of the sample
+		return prediction
+
+	def display(self):
+		# display the tree somehow? low priority task
