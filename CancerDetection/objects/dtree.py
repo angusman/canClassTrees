82b7456affb6e1ef7faa5a8a79260e906fdb2efc
Nick Sullivan <nbsullivan@gmail.com>
2016-07-06 19:42:58 -0700

    added more functions to the dtree skelleton

--- CancerDetection/objects/dtree.py
+++ CancerDetection/objects/dtree.py
@@ -1,4 +1,4 @@
-class dtree(object)
+class dtree_node(object)
 	"""
 	Basic decision tree object
 	"""
@@ -6,9 +6,25 @@ class dtree(object)
 		if data != None:
 			self.load_data(data)
 
-	def load_data(self, data = None):
+	def create_node(self, data = None):
 		# load data here
+
+	def set_entropy(self, data = None, Column = None, Value = None):
+		# for a given set calculate the entrop of a set
+
+		return entropy
+
+	def find_cutoff_value(self, datacol = None):
+		# for a given datacol find the aveage value and return this as the cut off value of splitting
+
+		return value
 	
+	def find_best_split(self, data = None):
+		# send in the entire dataset at this node level, returns the best sets to return on.
+
+
+		return leftset, rightset
+
 	def predict(self, sample):
 		# predict the state of the sample
 		return prediction
