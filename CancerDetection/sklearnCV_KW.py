d766885ed25871bc123e8144d9f21c279ff118cb
kwinterling <kwinterling@gmail.com>
2016-07-25 19:55:09 -0700

    Made Fixes
    Both bladder and leuk work. Must fix loading for reduced and add colon.

--- CancerDetection/sklearnCV_KW.py
+++ CancerDetection/sklearnCV_KW.py
@@ -13,9 +13,9 @@ from sklearn.ensemble import BaggingClassifier
 
 def load_reduced():
     """Load Reduced dataset. Return a dict"""
-    data = np.genfromtxt('data/DNA/ReducedDataSet_dataonly.csv')
-    X = np.transpose(data[1:, :-10])
-    Y = np.transpose(data[0, :-10])
+    data = np.genfromtxt('data/DNA/ReducedDataSet_dataonly.csv').transpose()
+    X = data[:, 1:-1]
+    Y = data[:, 0]
     dset = {}
     dset['observations'] = X
     dset['labels'] = Y
@@ -23,7 +23,7 @@ def load_reduced():
     return dset
 
 def load_leuk():
-    data = np.genfromtxt('data/DNA/labeled_leuk_corrected.csv',
+    data = np.genfromtxt('data/DNA/labeled_leuk.csv',
                         delimiter = ',', skip_header = 1)
     X = data[:, 1:-1]
     Y = data[:, -1]
@@ -59,27 +59,29 @@ def analyze_data(dataset):
               'randomforest': {},
               'adaboost': {}}
     
-    classifiers = {'dtree': skl.tree.DecisionTreeClassifier(),
-                   'bagging': BaggingClassifier(),
-                   'randomforest': RandomForestClassifier()}
+    classifiers = {'dtree': skl.tree.DecisionTreeClassifier,
+                   'bagging': BaggingClassifier,
+                   'randomforest': RandomForestClassifier,
+                   'adaboost': AdaBoostClassifier}
     cvs = {'KFold3': 3,
           'KFold5': 5,
-          'KFold7': 7,
-          'KFold10': 10,
-          'KFold13': 13}
+          'KFold7': 7}
     
     for algorithm, value in results.iteritems():
         errordt = {}
         for key, clf in classifiers.iteritems():
-            print clf
             classfd = {}
             for cvkey, cvnum in cvs.iteritems():
-                C = skl.tree.DecisionTreeClassifier()
+                if key == 'dtree':
+                    C = clf()
+                else:
+                    C = clf(n_estimators = 10)
                 classfd[cvkey] = sklcv.cross_val_score(C, X, Y, cv=cvnum)
             errordt[key] = classfd
         results[algorithm] = errordt
     
     return results
 
-analyze_data('bladder')
+dtt = analyze_data('reduced')
 
+print dtt
