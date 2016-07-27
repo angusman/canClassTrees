e524783cf0afed8c48a7a7359e1175aa7f022ab6
kwinterling <kwinterling@gmail.com>
2016-07-26 19:47:13 -0700

    SKLearn Code and Data
    Base SKLearn script with output to file. See skldt.json for an example
    of experiments. Reduced is not loading correctly.

--- CancerDetection/sklearnCV_KW.py
+++ CancerDetection/sklearnCV_KW.py
@@ -1,13 +1,16 @@
 
+import os
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn as skl
 import sklearn.cross_validation as sklcv
-from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import BaggingClassifier
 
+from pprint import pprint as pp
+from time import time
 
 """Each function returns a dictionary of observations and labels"""
 
@@ -43,45 +46,142 @@ def load_bladder():
     dset['labels'] = Y
     return dset
 
-def analyze_data(ensemble_n_estimators, dataset):
+def load_colon():
+    data = np.genfromtxt('data/DNA/labeled_colon.csv',
+                        delimiter = ',', skip_header = 1)
+    X = data[:, 1:-1]
+    Y = data[:, -1]
+    dset = {}
+    dset['observations'] = X
+    dset['labels'] = Y
+    return dset
+
+def load_liver():
+    data = np.genfromtxt('data/DNA/labeled_liver.csv',
+                        delimiter = ',', skip_header = 1)
+    X = data[:, 1:-1]
+    Y = data[:, -1]
+    dset = {}
+    dset['observations'] = X
+    dset['labels'] = Y
+    return dset
+
+
+
+def fix_y(nfolds, y):
+    return y
+
+
+def do_classification(clfname, clf, x, y, cvnum, esizes):
+    res = []
+    print clfname
+    if clfname == 'dtree':
+        C = clf()
+        start = time()
+        err = sklcv.cross_val_score(C, x, y, cv=cvnum)
+        stop = time()
+        result = {}
+        result['method'] = clfname
+        result['trees'] = 1
+        result['error'] = err
+        result['mean'] = err
+        result['min'] = err
+        result['max'] = err
+        result['std'] = 0
+        result['computationtime'] = stop - start
+        
+        res.append(result)
+    else:
+        itr = iter(esizes)
+        for n_trees in itr:
+            C = clf(n_estimators = n_trees)
+            start = time()
+            err = sklcv.cross_val_score(C, x, y, cv=cvnum)
+            stop = time()
+            result = {}
+            result['method'] = clfname
+            result['trees'] = n_trees
+            result['error'] = err
+            result['mean'] = np.mean(err)
+            result['min'] = np.min(err)
+            result['max'] = np.max(err)
+            result['std'] = np.std(err)
+            result['computationtime'] = stop - start
+            res.append(result)
+        
+    return res
+
+
+def analyze_data(ensemble_sizes, dataset):
     """Produce a dict structure of error and runtime values."""
+    
     dsets = {'reduced': load_reduced,
             'leuk': load_leuk,
-            'bladder': load_bladder}
+            'bladder': load_bladder,
+            'colon': load_colon,
+            'liver': load_liver}
     dstf = dsets[dataset]
     dset = dstf()
     X = dset['observations']
     Y = dset['labels']
     num_obs = X.shape[0]
+    num_feats = X.shape[1]
+
     
     results = {'dtree': {},
               'bagging': {},
               'randomforest': {},
-              'adaboost': {}}
+              'adaboost': {},}
     
     classifiers = {'dtree': skl.tree.DecisionTreeClassifier,
                    'bagging': BaggingClassifier,
                    'randomforest': RandomForestClassifier,
                    'adaboost': AdaBoostClassifier}
-    cvs = {'KFold3': 3,
-          'KFold5': 5,
-          'KFold7': 7}
+    cvs = {'KFold1': 2,
+          'KFold2': 3,
+          'KFold3': 5}
     
+    algs = ['adaboost', 'bagging', 'dtree', 'randomforest']
+    for algorithm in iter(algs):
+        clf = classifiers[algorithm]
+        cvruns = {}
+        
+        for cv, cvnum in cvs.iteritems():
+            cvruns[cv] = do_classification(algorithm, clf, X, Y, cvnum, ensemble_sizes)
+        
+        results[algorithm] = cvruns
+    
+    return results
+
+
+"""    
     for algorithm, value in results.iteritems():
         errordt = {}
         for key, clf in classifiers.iteritems():
             classfd = {}
             for cvkey, cvnum in cvs.iteritems():
-                if key == 'dtree':
-                    C = clf()
-                else:
-                    C = clf(n_estimators = ensemble_n_estimators)
-                classfd[cvkey] = sklcv.cross_val_score(C, X, Y, cv=cvnum)
+                classfd[cvkey] = do_classification(key, clf, X, Y, 4)
             errordt[key] = classfd
         results[algorithm] = errordt
+"""
+
+def to_file(fname):
+    dl = ['bladder', 'colon', 'leuk', 'liver']
+    dct = {}
+    num_trees = [2, 5, 10, 20]
+    for dset in iter(dl):
+        dct[dset] = analyze_data(num_trees, dset)
     
-    return results
+    fl = open(fname, 'w+')
+    pp(dct, stream=fl)
+    fl.close()
 
-dtt = analyze_data('reduced')
+def to_spreadsheet(resdict):
+    """Make Pandas DataFrames, resdict is output from analyze_data()."""
+    dfs = []
 
-print dtt
+to_file('skldt.json')
+
+# num_trees = [2, 5, 10, 20] # add 60, 100 on faster machines
+# dtt = analyze_data(num_trees, 'bladder')
+# pp(dtt)
