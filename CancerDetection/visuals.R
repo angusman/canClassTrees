1bbb66886354ee88f82dcfa811c01eae95c2cb90
Nick Sullivan <nbsullivan@gmail.com>
2016-07-27 11:45:36 -0700

    graphs made with karls results

--- CancerDetection/visuals.R
+++ CancerDetection/visuals.R
@@ -4,46 +4,48 @@ setwd('/Users/nick/Documents/MATLAB/canClassTrees/CancerDetection/data/results')
 library(ggplot2)
 
 temp = list.files(pattern="*.csv")
-
-full_df = data.frame(X = numeric(0), aveaccuracy = numeric(0), cancertype = numeric(0),
-                     fold0 = numeric(0), fold1 = numeric(0), fold2 = numeric(0),
-                     fold3 = numeric(0), fold4 = numeric(0), ntrees = numeric(0), 
-                     maxsinglefold = numeric(0), minsinglefold = numeric(0), std = numeric(0),
-                     timestamp, computationtime = numeric(0))
-
-for (i in temp){
-  print(i)
-  temp_df <- read.csv(file = i, head = TRUE)
-  full_df <- rbind(full_df,temp_df)
-}
+homebrew <- read.csv(temp[1])
+sklearn <- read.csv(temp[2])
+sklearn <- subset(sklearn, kfolds ==5 & method == 'randomforest')
 
 
-ggplot(data = full_df, aes(x = ntrees, y = aveaccuracy, color = cancertype)) +
+keeps <- c('aveaccuracy', 'computationtime', 'cancertype', 'maxsinglefold', 'method', 'minsinglefold' , 'std', 'ntrees')
+sklearn <- sklearn[keeps]
+homebrew <- homebrew[keeps]
+
+full_df <- rbind(sklearn,homebrew)
+
+ggplot(data = full_df, aes(x = ntrees, y = aveaccuracy, color = method)) +
   geom_point() +
   geom_line() + 
   scale_x_continuous(breaks = full_df$ntrees) +
   labs(x = 'Number of trees', y = 'Average Accuracy', title = '5 Fold CV') +
   facet_wrap(~ cancertype)
+ggsave('AverageaccHBvSK.pdf')
 
-ggplot(data = full_df, aes(x = ntrees, y = maxsinglefold, color = cancertype)) +
+
+ggplot(data = full_df, aes(x = ntrees, y = maxsinglefold, color = method)) +
   geom_point() +
   geom_line() + 
   scale_x_continuous(breaks = full_df$ntrees) +
   labs(x = 'Number of trees', y = 'Max accuracy for a single fold', title = 'Max of Accuracy of the 5 Folds') +
   facet_wrap(~ cancertype)
+ggsave('MaxAccHBvSK.pdf')
 
-ggplot(data = full_df, aes(x = ntrees, y = std, color = cancertype)) +
+ggplot(data = full_df, aes(x = ntrees, y = std, color = method)) +
   geom_point() +
   geom_line() + 
   scale_x_continuous(breaks = full_df$ntrees) +
   labs(x = 'Number of trees', y = 'Standard deviation of fold results', title = '5 Fold CV STD') +
   facet_wrap(~ cancertype)
+ggsave('StdHBvSK.pdf')
   
-ggplot(data = full_df, aes(x = ntrees, y = aveaccuracy, color = cancertype)) +
+ggplot(data = full_df, aes(x = ntrees, y = aveaccuracy, color = method)) +
   geom_point() +
   geom_line() + 
   labs(x = 'Number of trees', y = 'Fold accuracy', title = '5 Fold CV accuracy') +
   facet_wrap(~ cancertype) +
   scale_x_continuous(breaks = full_df$ntrees) +
-  geom_line(linetype = 2, aes(y = maxsinglefold, colour = "max fold acc")) + 
-  geom_line(linetype = 2, aes(y = minsinglefold, colour = "min fold acc"))
+  geom_line(linetype = 2, aes(y = maxsinglefold)) + 
+  geom_line(linetype = 2, aes(y = minsinglefold))
+ggsave('AccBoundsHBvSK.pdf')
