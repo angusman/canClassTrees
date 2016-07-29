36dbd506596850ce843b64371bb7d06c675b9628
Nick Sullivan <nbsullivan@gmail.com>
2016-07-29 15:08:29 -0700

    fixed casestudy1a visuals, dtree has been removed from graphics

--- CancerDetection/visualsnew.R
+++ CancerDetection/visualsnew.R
@@ -3,6 +3,8 @@ setwd('/Users/nick/Documents/MATLAB/canClassTrees/CancerDetection/data/results')
 library(ggplot2)
 
 r1 <- read.csv("casestudy1.csv")
+r1a <- read.csv("casestudy1a.csv")
+r1a <- subset(r1a, method != "DecisionTreeClassifier")
 r2 <- read.csv("casestudy2.csv")
 r3 <- read.csv("casestudy3.csv")
 r4 <- read.csv("casestudy4.csv")
@@ -35,6 +37,33 @@ ggplot(data = r1, aes(x = ntrees, y = std, color = method)) +
   labs(x = 'Number of trees', y = 'Stand deviatation of fold accuracy', title = 'Random Forest and Adaboost STD') +
   facet_wrap(~ cancertype)
 ggsave("casestudyvis/c1std.pdf")
+
+# case study 1a
+
+ggplot(data = r1a, aes(x = ntrees, y = aveaccuracy, color = method)) +
+  geom_point() +
+  geom_line() + 
+  scale_x_continuous(breaks = full_df$ntrees) +
+  labs(x = 'Number of trees', y = 'Average Accuracy', title = '10 Fold CV') +
+  facet_wrap(~ cancertype)
+ggsave("casestudyvis/c1Aaccuracy.pdf")
+
+ggplot(data = r1a, aes(x = ntrees, y = computationtime, color = method)) +
+  geom_point() +
+  geom_line() + 
+  scale_x_continuous(breaks = full_df$ntrees) +
+  labs(x = 'Number of trees', y = 'Computation Time', title = 'Random Forest and Adaboost Computation') +
+  facet_wrap(~ cancertype)
+ggsave("casestudyvis/c1Acputime.pdf")
+
+
+ggplot(data = r1a, aes(x = ntrees, y = std, color = method)) +
+  geom_point() +
+  geom_line() + 
+  scale_x_continuous(breaks = full_df$ntrees) +
+  labs(x = 'Number of trees', y = 'Stand deviatation of fold accuracy', title = 'Random Forest and Adaboost Computation STD') +
+  facet_wrap(~ cancertype)
+ggsave("casestudyvis/c1Astd.pdf")
 
 # case study 2
 
@@ -85,10 +114,10 @@ ggsave("casestudyvis/c5BootCPU.pdf")
 
 ggplot(data = r6, aes(x = cancertype, y = aveaccuracy, fill = method)) +
   geom_bar(stat = "identity", width = .7, position = "dodge") +
-  labs(x = "Cancer Type", y = "Average Accuracy", title = "Accuracy of 3 Methods" )
+  labs(x = "Cancer Type", y = "Average Accuracy", title = "Accuracy of 4 Methods" )
 ggsave("casestudyvis/c6acc3method.pdf")
 
 ggplot(data = r6, aes(x = cancertype, y = computationtime, fill = method)) +
   geom_bar(stat = "identity", width = .7, position = "dodge") +
-  labs(x = "Cancer Type", y = "Computation Time", title = "Computation Time of 3 Methods" )
+  labs(x = "Cancer Type", y = "Computation Time", title = "Computation Time of 4 Methods" )
 ggsave("casestudyvis/c6CPU3method.pdf")
